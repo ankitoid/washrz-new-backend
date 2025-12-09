9184da17ae1b2b0a1a2421f0bb25731ad06f6ecd
Ayush Singh <ayush.singh@gxinetworks.com>
2025-12-09 12:16:54 +0530

    new changes after delete

---
+++ database.js
@@ -0,0 +1,5 @@
+import mongoose from "mongoose";
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DataBase Connected!:)"))
+  .catch((err) => console.log("hii error", err));
---
+++ routes/customerRoutes.js
@@ -0,0 +1,45 @@
+import express from "express";
+import {
+  addCustomer,
+  addOrder,
+  addPickup,
+  addSchedulePickup,
+  changeOrderStatus,
+  completePickup,
+  deleteOrderById,
+  deletePickup,
+  getAssignedPickups,
+  getCancelPickups,
+  getCustomers,
+  getOrderByOrderId,
+  getOrderTotalBill,
+  getOrders,
+  getOrdersByFilter,
+  getPickups,
+  getSchedulePickups,
+  updateOrderById,
+} from "../controller/customerController.js";
+const router = express.Router();
+
+router.post("/addPickup", addPickup);
+router.get("/getPickups", getPickups);
+router.get("/getAssignedPickups", getAssignedPickups);
+router.post("/addSchedulePickup", addSchedulePickup);
+router.get("/getSchedulePickups", getSchedulePickups);
+router.post("/addCustomer", addCustomer);
+router.get("/getCustomers", getCustomers);
+router.put("/deletePickup/:id", deletePickup);
+router.put("/completePickup/:id", completePickup);
+
+router.post("/addOrder", addOrder);
+router.get("/getOrders", getOrders);
+router.get("/getOrdersByFilter", getOrdersByFilter);
+router.get("/getOrderBill/:number", getOrderTotalBill);
+router.get("/getCancelPickups", getCancelPickups);
+router.put("/changeOrderStatus/:id", changeOrderStatus);
+
+router.post("/get_order_by_id", getOrderByOrderId);
+router.put("/update_order_by_id/:id", updateOrderById);
+router.delete("/delete_order_by_id/:id", deleteOrderById);
+
+export { router as default };
