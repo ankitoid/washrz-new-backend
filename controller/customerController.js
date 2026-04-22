4668b59c766e7a5957ef17435fe19fd9f4f26f5a
Ayush Singh <ayush.singh@gxinetworks.com>
2026-04-22 15:56:14 +0530

    changes for adding the model path in the controller for the catalog item

--- controller/customerController.js
+++ controller/customerController.js
@@ -13,6 +13,7 @@ import multer from "multer";
 import customerFcmService from "../services/customerFcmService.js";
 import { createCustomerNotification } from "./customerNotificationController.js";
 import CustomerNotification from "../models/customerNotificationSchema.js";
+import CatalogItem from "../models/catalogItemSchema.js";
 
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
