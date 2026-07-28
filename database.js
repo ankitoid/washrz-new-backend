20374850d92c55a066b5c8e7197dcaa1b6b25b9c
shubhsingh1515 <shubhsingh1515@gmail.com>
2026-07-28 16:54:33 +0530

    revert database.js code

--- database.js
+++ database.js
@@ -1,13 +1,13 @@
-// import mongoose from "mongoose";
-// mongoose
-//   .connect(process.env.MONGO_URI)
-//   .then(() => console.log("DataBase Connected!:)"))
-//   .catch((err) => console.log("hii error", err));
-
 import mongoose from "mongoose";
-import dns from "node:dns/promises";
-dns.setServers(["1.1.1.1"]); 
-await mongoose
+mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("DataBase Connected!:)"))
   .catch((err) => console.log("hii error", err));
+
+// import mongoose from "mongoose";
+// import dns from "node:dns/promises";
+// dns.setServers(["1.1.1.1"]); 
+// await mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => console.log("DataBase Connected!:)"))
+//   .catch((err) => console.log("hii error", err));
